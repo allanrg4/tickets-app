5a0d918d87ce318cb5a353d56311a5c63e8d8908
Jose Miguel <jmqc77@gmail.com>
2025-05-07 08:12:06 -0600

    add a all front (#3)

--- tickets-app-frontend/src/app/pages/agent/agent.component.ts
+++ tickets-app-frontend/src/app/pages/agent/agent.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { TicketsFormComponent } from "../../components/tickets-form/tickets-form.component";
+import { TicketsQueueComponent } from "../../components/tickets-queue/tickets-queue.component";
 
 @Component({
   selector: 'app-agent',
-  imports: [],
+  imports: [TicketsFormComponent, TicketsQueueComponent],
   templateUrl: './agent.component.html',
   styles: ``
 })
