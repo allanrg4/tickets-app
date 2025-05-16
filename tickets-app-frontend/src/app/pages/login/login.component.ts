33cead676fc8feda01cd869750c3c8d7699a0fdd
JoseMi317 <jmqc77@gmail.com>
2025-05-15 21:10:35 -0600

    tickets service base

--- tickets-app-frontend/src/app/pages/login/login.component.ts
+++ tickets-app-frontend/src/app/pages/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { User } from '../../entities/user';
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -9,20 +11,40 @@ import { AuthService } from '../../services/auth.service';
 })
 export class LoginComponent {
   readonly service = inject(AuthService);
+  readonly router = inject(Router);
+
   username = '';
   password = '';
 
   onSubmit() {
     this.service.login(this.username, this.password).subscribe({
-      next: (res) => {
+      next: (res: any) => {
         console.log('Login exitoso:', res);
-        // redirigir o guardar token si es necesario
+
+        const { token, username, role, id } = res;
+
+        localStorage.setItem('token', token);
+        const user: User = { id, username, role };
+        localStorage.setItem('user', JSON.stringify(user));
+
+        switch (role) {
+          case 'admin':
+            this.router.navigate(['/admin']);
+            break;
+          case 'resolver':
+            this.router.navigate(['/resolver']);
+            break;
+          case 'agent':
+            this.router.navigate(['/agent']);
+            break;
+          default:
+            this.router.navigate(['/']);
+            break;
+        }
       },
       error: (err) => {
         console.error('Error en login:', err);
-        // mostrar mensaje de error si deseas
       }
     });
   }
-
 }
